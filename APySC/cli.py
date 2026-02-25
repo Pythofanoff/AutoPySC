dc51735f70ccb6fcb33fb2daf1fce1ba8ec7a85c
Pythofanoff <you@example.com>
2026-02-26 00:13:06 +0500

    try 2

--- APySC/cli.py
+++ APySC/cli.py
@@ -1,11 +1,13 @@
-from __main__ import 
-def start():
-    import argparse, runpy 
 
-    parser = argparse.ArgumentParser(prog="apysc")
-    parser.add_argument("--version", action="version", version="0.1.0")
-    parser.add_argument("start", nargs="?", action=main())
-    args = parser.parse_args()
+# def start():
+#     import argparse
+
+#     parser = argparse.ArgumentParser(prog="apysc")
+#     parser.add_argument("--version", action="version", version="0.1.0")
+#     parser.add_argument("start", nargs="?", action=main())
+#     args = parser.parse_args()
 
 def main():
+    import runpy
+    
     runpy.run_module("mypkg", run_name="__main__", alter_sys=True)
