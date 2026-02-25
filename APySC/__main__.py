6099a8864273b45225ee8fa81080bf788981da5d
Pythofanoff <you@example.com>
2026-02-25 19:27:01 +0500

    update main.py

--- APySC/__main__.py
+++ APySC/__main__.py
@@ -5,7 +5,50 @@ try:
 except ImportError as e: 
     print(f"Not enough trace dependencies: {e}")
    
- 
+def enum_paths(target: tuple):
+    global skipped, created, modified, start
+
+    for path in target:
+        if path in PATH_FLD:
+            # Создание папок
+            if os.path.exists(path):
+                if REPLACE_EXISTS_FOLDERS:
+                    os.makedirs(path, exist_ok=True)
+                    print(f'[Spent: {round(time.time() - start, 3)}]: Create {path} <- EDIT')
+                    modified += 1
+                elif os.path.exists(path): 
+                    # Папка уже существует
+                    print(f"[Spent: {round(time.time() - start, 3)}]: The following files already exist: Create {path} <- SKIP")
+                    skipped += 1
+            else: 
+                os.makedirs(path, exist_ok=True)
+                print(f'[Spent: {round(time.time() - start, 3)}]: Create {path}')  
+                created += 1
+        else: 
+            # Создание файлов
+            if os.path.exists(path):
+                if REPLACE_EXISTS_FILE: 
+                    if path != '.gitignore':
+                        open(path, "w", encoding="UTF-8").write('')
+                        print(f'[Spent: {round(time.time() - start, 3)}]: Create {path} <- EDIT')
+                        modified += 1
+                    elif os.path.exists('./venv'):
+                        open('.gitignore', "w", encoding="UTF-8").write(CNST_TXT)
+                        print(f'[Spent: {round(time.time() - start, 3)}]: Create {path} <- EDIT')
+                        modified += 1
+                    elif not os.path.exists('./venv'): 
+                        open('.gitignore', "w", encoding="UTF-8").write('')
+                        print(f'[Spent: {round(time.time() - start, 3)}]: Create {path} <- EDIT')
+                        modified += 1
+                else:
+                    # Файл уже существует
+                    print(f"[Spent: {round(time.time() - start, 3)}]: The following files already exist: Create {path} <- SKIP")
+                    skipped += 1
+            else:
+                open(path, "w", encoding="UTF-8").write('')
+                print(f'[Spent: {round(time.time() - start, 3)}]: Create {path}')
+                created += 1
+
 if __name__ == "__main__":
     skipped = 0
     created = 0
@@ -14,50 +57,6 @@ if __name__ == "__main__":
     sep = '-' * 15 
 
     start = time.time() 
-
-    def enum_paths(target: tuple):
-        global skipped, created, modified, start
-
-        for path in target:
-            if path in PATH_FLD:
-                # Создание папок
-                if os.path.exists(path):
-                    if REPLACE_EXISTS_FOLDERS:
-                        os.makedirs(path, exist_ok=True)
-                        print(f'[Spent: {round(time.time() - start, 3)}]: Create {path} <- EDIT')
-                        modified += 1
-                    elif os.path.exists(path): 
-                        # Папка уже существует
-                        print(f"[Spent: {round(time.time() - start, 3)}]: The following files already exist: Create {path} <- SKIP")
-                        skipped += 1
-                else: 
-                    os.makedirs(path, exist_ok=True)
-                    print(f'[Spent: {round(time.time() - start, 3)}]: Create {path}')  
-                    created += 1
-            else: 
-                # Создание файлов
-                if os.path.exists(path):
-                    if REPLACE_EXISTS_FILE: 
-                        if path != '.gitignore':
-                            open(path, "w", encoding="UTF-8").write('')
-                            print(f'[Spent: {round(time.time() - start, 3)}]: Create {path} <- EDIT')
-                            modified += 1
-                        elif os.path.exists('./venv'):
-                            open('.gitignore', "w", encoding="UTF-8").write(CNST_TXT)
-                            print(f'[Spent: {round(time.time() - start, 3)}]: Create {path} <- EDIT')
-                            modified += 1
-                        elif not os.path.exists('./venv'): 
-                            open('.gitignore', "w", encoding="UTF-8").write('')
-                            print(f'[Spent: {round(time.time() - start, 3)}]: Create {path} <- EDIT')
-                            modified += 1
-                    else:
-                        # Файл уже существует
-                        print(f"[Spent: {round(time.time() - start, 3)}]: The following files already exist: Create {path} <- SKIP")
-                        skipped += 1
-                else:
-                    open(path, "w", encoding="UTF-8").write('')
-                    print(f'[Spent: {round(time.time() - start, 3)}]: Create {path}')
-                    created += 1
  
     print('''
     ░█████╗░██████╗░██╗░░░██╗░██████╗░█████╗░ 
